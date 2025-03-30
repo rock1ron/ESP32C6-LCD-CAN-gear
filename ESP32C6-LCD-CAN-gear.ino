948535ed0e1458ee67dc4f8995db01f7a96aadd8
rock1ron <94650274+rock1ron@users.noreply.github.com>
2025-03-30 23:40:48 +0200

    .

--- ESP32C6-LCD-CAN-gear.ino
+++ ESP32C6-LCD-CAN-gear.ino
@@ -28,18 +28,20 @@
 #define TFT_WIDTH   172
 #define TFT_HEIGTH  320
 
-#define CAN_EspeedL_1000 0xA0 // 1000 rpm
-#define CAN_EspeedH_1000 0x0F
-#define CAN_VspeedL_0xAA_20 0xC8 // 20 km/h
-#define CAN_VspeedH_0xAA_20 0x00
+#define CAN_VspeedL_0xAA_5kmh 0x30
+#define CAN_VspeedH_0xAA_5kmh 0x00
+#define CAN_VspeedL_0xAA_50kmh 0xE8
+#define CAN_VspeedH_0xAA_50kmh 0x01
+#define CAN_VspeedL_0xAA_150kmh 0xB0
+#define CAN_VspeedH_0xAA_150kmh 0x05
 
-#define ChkSumOffset_AA   245 
 #define ChkSumOffset_C4   108
-#define ChkSumOffset_C8   108
 #define ChkSumOffset_130   11 
-#define ChkSumOffset_130_B 79
 #define ChkSumOffset_1A0  162
-#define ChkSumOffset_1D0   94
+
+const int _0xC4_data[] = {0xBD, 0x02, 0xFC, 0x00, 0x00, 0xFF, 0xF1};
+const int _0x130_data[] = {0x45, 0x43, 0x29, 0x8F};
+const int _0x1A0_data[] = {0xF0, 0x10, 0x00, 0x00, 0x00, 0x00, 0x08};
 
 // OPTION 1 (recommended) is to use the HARDWARE SPI pins, which are unique
 // to each board and not reassignable. For Arduino Uno: MOSI = pin 11 and
@@ -55,40 +57,23 @@
 
 Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST);
 
-int ChkSum_AA, ChkSum_C4, ChkSum_C8, ChkSum_130, ChkSum_1A0, ChkSum_1D0 = 0; 
-int MsgCtr_AA, MsgCtr_C4, MsgCtr_C8, MsgCtr_130, MsgCtr_1A0, MsgCtr_1D0 = 0;
+int ChkSum_C4, ChkSum_130, ChkSum_1A0 = 0; 
+int MsgCtr_C4, MsgCtr_130, MsgCtr_1A0 = 0;
 
 CanFrame rxFrame;
-
-
-void send_0xAA_Frame(uint8_t AccPedal) { // 20 ms // AccPedal
-	CanFrame _0xAA_Frame = { 0 };
-	_0xAA_Frame.identifier = 0xAA; // 170
-	_0xAA_Frame.extd = 0;
-	_0xAA_Frame.data_length_code = 8;
-	_0xAA_Frame.data[0] = ChkSum_AA;
-	_0xAA_Frame.data[1] = 0x40 + MsgCtr_AA;
-	_0xAA_Frame.data[2] = 0x1A;
-	_0xAA_Frame.data[3] = 0x5C;    
-	_0xAA_Frame.data[4] = 0x00; //CAN_EspeedL_1000;   
-	_0xAA_Frame.data[5] = 0x00; //CAN_EspeedH_1000;   
-	_0xAA_Frame.data[6] = 0x94;
-	_0xAA_Frame.data[7] = 0x00;
-  ESP32Can.writeFrame(_0xAA_Frame);  // timeout defaults to 1 ms
-}
 
 void send_0xC4_Frame(uint8_t SteerAng) { // 10 ms // SteeringWheelAngle
 	CanFrame _0xC4_Frame = { 0 };
 	_0xC4_Frame.identifier = 0xC4; // 196
 	_0xC4_Frame.extd = 0;
 	_0xC4_Frame.data_length_code = 7;
-	_0xC4_Frame.data[0] = 0xBD;
-	_0xC4_Frame.data[1] = 0x02;
-	_0xC4_Frame.data[2] = 0xFC;
-	_0xC4_Frame.data[3] = 0x00;    
-	_0xC4_Frame.data[4] = 0x00;   
-	_0xC4_Frame.data[5] = 0xFF;   
-	_0xC4_Frame.data[6] = 0xF1;   
+	_0xC4_Frame.data[0] = _0xC4_data[0];
+	_0xC4_Frame.data[1] = _0xC4_data[1];
+	_0xC4_Frame.data[2] = _0xC4_data[2];
+	_0xC4_Frame.data[3] = _0xC4_data[3];    
+	_0xC4_Frame.data[4] = _0xC4_data[4];   
+	_0xC4_Frame.data[5] = _0xC4_data[5];   
+	_0xC4_Frame.data[6] = _0xC4_data[6];   
   ESP32Can.writeFrame(_0xC4_Frame);  // timeout defaults to 1 ms
 }
 
@@ -97,44 +82,28 @@ void send_0x130_Frame(uint8_t TerminalStatus) { // 100 ms // TerminalStatus
 	_0x130_Frame.identifier = 0x130; // 304
 	_0x130_Frame.extd = 0;
 	_0x130_Frame.data_length_code = 5;
-	_0x130_Frame.data[0] = 0x45;
-	_0x130_Frame.data[1] = 0x43;
-	_0x130_Frame.data[2] = 0x29;
-	_0x130_Frame.data[3] = 0x8F;    
-	_0x130_Frame.data[4] = ChkSum_130; // H nibble is checksum and L nibble is counter
+	_0x130_Frame.data[0] = _0x130_data[0];
+	_0x130_Frame.data[1] = _0x130_data[1];
+	_0x130_Frame.data[2] = _0x130_data[2];
+	_0x130_Frame.data[3] = _0x130_data[3];    
+	_0x130_Frame.data[4] = (ChkSum_130 * 0x10) + MsgCtr_130; // H-nibble is checksum and L-nibble is counter
   ESP32Can.writeFrame(_0x130_Frame);  // timeout defaults to 1 ms
 }
 
-void send_0x1A0_Frame(uint8_t Speed) { // 160 ms // Speed
+void send_0x1A0_Frame(uint8_t Speed) { 
 	CanFrame _0x1A0_Frame = { 0 };
 	_0x1A0_Frame.identifier = 0x1A0; // 416
 	_0x1A0_Frame.extd = 0;
 	_0x1A0_Frame.data_length_code = 8;
-	_0x1A0_Frame.data[0] = 0xF0;
-	_0x1A0_Frame.data[1] = 0x10;
-	_0x1A0_Frame.data[2] = 0x00;
-	_0x1A0_Frame.data[3] = 0x00;    
-	_0x1A0_Frame.data[4] = 0x00;   
-	_0x1A0_Frame.data[5] = 0x00;   
-	_0x1A0_Frame.data[6] = (MsgCtr_1A0 * 0x10) + 0x08; // H-nibble is counter
+	_0x1A0_Frame.data[0] = _0x1A0_data[0];
+	_0x1A0_Frame.data[1] = _0x1A0_data[1];
+	_0x1A0_Frame.data[2] = _0x1A0_data[2];
+	_0x1A0_Frame.data[3] = _0x1A0_data[3];    
+	_0x1A0_Frame.data[4] = _0x1A0_data[4];   
+	_0x1A0_Frame.data[5] = _0x1A0_data[5];   
+	_0x1A0_Frame.data[6] = (MsgCtr_1A0 * 0x10) + _0x1A0_data[6]; // H-nibble is counter
 	_0x1A0_Frame.data[7] = ChkSum_1A0;
   ESP32Can.writeFrame(_0x1A0_Frame);  // timeout defaults to 1 ms
-}
-
-void send_0x1D0_Frame(uint8_t EngineData) { // 200 ms // EngineData
-	CanFrame _0x1D0_Frame = { 0 };
-	_0x1D0_Frame.identifier = 0x1D0; // 464
-	_0x1D0_Frame.extd = 0;
-	_0x1D0_Frame.data_length_code = 8;
-	_0x1D0_Frame.data[0] = 0x7C;
-	_0x1D0_Frame.data[1] = 0xFF;
-	_0x1D0_Frame.data[2] = 0x40 + MsgCtr_1D0;
-	_0x1D0_Frame.data[3] = 0xB1;    
-	_0x1D0_Frame.data[4] = 0x2D;   
-	_0x1D0_Frame.data[5] = 0xB6;   
-	_0x1D0_Frame.data[6] = 0xCC;
-	_0x1D0_Frame.data[7] = 0xA6;
-  ESP32Can.writeFrame(_0x1D0_Frame);  // timeout defaults to 1 ms
 }
 
 void setup(void) {
@@ -169,24 +138,13 @@ void setup(void) {
   tft.setTextSize(4);
    
   Serial.println(F("Initialized"));
-
 }
 
 void loop() {
   static int lastgear, lastlockup = 0;
   int gear, lockup = 0;
-  static uint32_t lastStamp_AA_20ms, lastStamp_C4_10ms, lastStamp_C8_200ms, lastStamp_130_100ms, lastStamp_1A0_160ms, lastStamp_1D0_200ms  = 0;
+  static uint32_t lastStamp_C4_10ms, lastStamp_130_100ms, lastStamp_1A0_20ms  = 0;
   uint32_t currentStamp = millis();
-/*
-  if(currentStamp - lastStamp_AA_20ms > 19) {   // sends frame every 20 ms
-      if (MsgCtr_AA < 14) MsgCtr_AA++; else MsgCtr_AA = 0;
-      ChkSum_AA = (MsgCtr_AA + ChkSumOffset_AA) % 0x100; // Test with precalculated values
-      lastStamp_AA_20ms = currentStamp;
-      send_0xAA_Frame(MsgCtr_AA);
-      Serial.print(lastStamp_AA_20ms);
-      Serial.print(" 0xAA \n\r");
-  }    
-*/
 /*
   if(currentStamp - lastStamp_C4_10ms > 9) {   // sends frame every 10 ms
       if (MsgCtr_C4 < 14) MsgCtr_C4++; else MsgCtr_C4 = 0;
@@ -207,25 +165,15 @@ void loop() {
       Serial.print(" 0x130 \n\r");
   }
 
-  if(currentStamp - lastStamp_1A0_160ms > 159) {   // sends frame every 160 ms
+  if(currentStamp - lastStamp_1A0_20ms > 19) {   // sends frame every 20 ms
       if (MsgCtr_1A0 < 14) MsgCtr_1A0++; else MsgCtr_1A0 = 0;
       ChkSum_1A0 = ((MsgCtr_1A0 * 0x10) + ChkSumOffset_1A0) % 0x100;  // Test with precalculated values
-      lastStamp_1A0_160ms = currentStamp;
+      lastStamp_1A0_20ms = currentStamp;
       send_0x1A0_Frame(MsgCtr_1A0);
-      Serial.print(lastStamp_1A0_160ms);
+      Serial.print(lastStamp_1A0_20ms);
       Serial.print(" 0x1A0 \n\r");
   }
 
-/*
-  if(currentStamp - lastStamp_1D0_200ms > 199) {   // sends frame every 200 ms
-      if (MsgCtr_1D0 < 14) MsgCtr_1D0++; else MsgCtr_1D0 = 0;
-      // ChkSum_1D0 = (MsgCtr_1D0 + ChkSumOffset_1D0) % 0x100;  // Test with precalculated values
-      lastStamp_1D0_200ms = currentStamp;
-      send_0x1D0_Frame(MsgCtr_1D0);
-      Serial.print(lastStamp_1D0_200ms);
-      Serial.print(" 0x1D0 \n\r");
-  }
-*/
 /*
   if(ESP32Can.readFrame(rxFrame, 100)) { // You can set custom timeout, default is 1000
        //Serial.printf("Received frame: %03X \r\n", rxFrame.identifier);
